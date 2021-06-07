fc0fbebd96a534bcf381353a664848f5e0b92d8d
ryoya49 <ryoya@occ.co.jp>
2021-06-07 15:16:15 +0900

    画面追加しました

---
+++ ThanksCardClient/Views/ThanksCardInBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ThanksCardClient.Views
+{
+    /// <summary>
+    /// Interaction logic for ThanksCardInBox
+    /// </summary>
+    public partial class ThanksCardInBox : UserControl
+    {
+        public ThanksCardInBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ThanksCardClient/Views/ThanksCardOutdetails.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ThanksCardClient.Views
+{
+    /// <summary>
+    /// Interaction logic for ThanksCardOutdetails
+    /// </summary>
+    public partial class ThanksCardOutdetails : UserControl
+    {
+        public ThanksCardOutdetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
