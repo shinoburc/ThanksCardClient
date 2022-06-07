7d7be43f8c79b4e0c203c9ea5dd1c19e61350b54
kkz1022 <kakazu-souta-1022@occ.co.jp>
2022-06-07 16:27:49 +0900

    サンクスしたよ

--- ThanksCardClient/ViewModels/registrationscreenViewModel.cs
+++ ThanksCardClient/ViewModels/registrationscreenViewModel.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿#nullable disable
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using ThanksCardClient.Models;
 namespace ThanksCardClient.ViewModels
 {
-    internal class RegistrationsScreenViewModel
+    public class registrationscreenViewModel : BindableBase
     {
-       
+        private readonly IRegionManager regionManager;
+
+        public registrationscreenViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+        #region ShowFooterCommand
+        private DelegateCommand _ShowFooterCommand;
+        public DelegateCommand ShowFooterCommand =>
+            _ShowFooterCommand ?? (_ShowFooterCommand = new DelegateCommand(ExecuteShowFooterCommand));
+
+        void ExecuteShowFooterCommand()
+        {
+            this.regionManager.Regions["FooterRegion"].RemoveAll();
+            this.regionManager.RequestNavigate("FooterRegion", nameof(Views.Footer));
+
+        }
+        #endregion
     }
 }
